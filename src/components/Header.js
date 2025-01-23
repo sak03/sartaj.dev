f8f92b89d75a79aecf5ad78c96e2343d98e3dcf6
sak03 <sartaj2394@gmail.com>
2025-01-24 00:05:13 +0530

    meta data added

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='brand-name'>Header</div>
+    <div className='px-3 py-2 flex justify-between bg-white'>
+      <div >
+        <h3 className='brand-name'>Sartaj Alam</h3>
+      </div>
+      <div className='flex'>
+        <span>Home</span>
+        <span>About</span>
+        <span>Work</span>
+        <span>Skills</span>
+        <span>Projects</span>
+        <span>Contact</span>
+      </div>
+    </div>
   )
 }
 
