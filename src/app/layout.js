6e260847bd7d45aee45276b6251e5a89329b6680
sak03 <sartaj2394@gmail.com>
2025-01-23 22:01:43 +0530

    scss package added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "../scss/style.scss"
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='brand-name'>Header</div>
   )
 }
 
