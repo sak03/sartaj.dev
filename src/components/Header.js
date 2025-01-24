9179b1d1d56057607db6e772ab352321682420dd
sak03 <sartaj2394@gmail.com>
2025-01-24 12:03:48 +0530

    updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import { FaHome, FaUser, FaUserAlt } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <div className='px-5 py-1 flex justify-between bg-white'>
+    <div className='px-5 flex justify-between bg-white'>
       <h3 className='pointer brand-name'><span className='brand-name1'>SARTAJ</span> <span className='brand-name2'>ALAM</span></h3>
-      <div className='flex my-1'>
-        <span className='header-item-active'>Home</span>
-        <span className='header-item mx-4'>About</span>
+      <div className='flex my-2'>
+        <span className='header-item-active flex'><FaHome className='mt-1 mx-1' style={{fontSize:"23px"}}/> Home</span>
+        <span className='header-item mx-4 flex'><FaUserAlt className='mt-2 mx-1' style={{ fontSize: "20px" }}/> About</span>
         <span className='header-item'>Work</span>
         <span className='header-item mx-4'>Skills</span>
         <span className='header-item'>Projects</span>
