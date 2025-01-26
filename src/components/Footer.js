a8e6913a2019a709e336b90df10a8fefe1ae1862
sak03 <sartaj2394@gmail.com>
2025-01-26 12:25:34 +0530

    footer updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,27 @@
 import React from 'react'
 
 const Footer = () => {
+  const currentDate = new Date()
   return (
-    <div>Footer</div>
+    <div className='bg-white'>
+      <div className='px-5 flex justify-between'>
+        <div>
+          <h3 className='footer-item'>Sartaj Alam</h3>
+          <p>Your complete Web Solutions</p>
+        </div>
+        <div>
+          <h3>Expert in</h3>
+          <p>Front End Web development</p>
+        </div>
+        <div>
+          <h3>Follow me</h3>
+          <p>Li</p>
+        </div>
+      </div>
+      <div>
+        <div className='footer-line'></div>
+        &copy; {currentDate.getFullYear()}</div>
+    </div>
   )
 }
 
