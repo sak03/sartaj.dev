9446e9a590b87a3ca356ea484354c50714f8d3a4
sak03 <sartaj2394@gmail.com>
2025-01-26 18:01:18 +0530

    header footer updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,26 +1,40 @@
 import React from 'react'
+import { FaGithubSquare, FaLinkedin, FaTwitterSquare } from "react-icons/fa";
 
 const Footer = () => {
   const currentDate = new Date()
   return (
     <div className='bg-white'>
-      <div className='px-5 flex justify-between'>
+      <div className='px-5 flex justify-between py-3 light-bg'>
         <div>
           <h3 className='footer-item'>Sartaj Alam</h3>
-          <p>Your complete Web Solutions</p>
+          <p className='footer-sub-item'>Your complete web solutions</p>
         </div>
         <div>
-          <h3>Expert in</h3>
-          <p>Front End Web development</p>
+          <h3 className='footer-item'>Expert in</h3>
+          <p className='footer-sub-item'>Front End, Full Stack</p>
+          <p className='footer-sub-item'>MERN Stack</p>
         </div>
         <div>
-          <h3>Follow me</h3>
-          <p>Li</p>
+          <h3 className='footer-item'>Important Links</h3>
+          <p className='footer-sub-item'>About</p>
+          <p className='footer-sub-item'>Work</p>
+        </div>
+        <div>
+          <h3 className='footer-item'>Follow me</h3>
+          <div className='flex'>
+          <a href='https://github.com/sak03' target='_blank' className='footer-sub-item-icon'><FaGithubSquare /></a>
+          <a href='https://www.linkedin.com/in/sak03/' target='_blank' className='footer-sub-item-icon mx-2'><FaLinkedin /></a>
+          <a href='https://x.com/sartaj_03' target='_blank' className='footer-sub-item-icon'><FaTwitterSquare /></a>
+          </div>
         </div>
       </div>
-      <div>
-        <div className='footer-line'></div>
-        &copy; {currentDate.getFullYear()}</div>
+      <div className='footer-line'></div>
+      <div className='px-5 flex justify-between py-3 light-bg'>
+        <span className='footer-bottom'> &copy; {currentDate.getFullYear()}</span>
+        <span className='footer-bottom'>Made with love ❤️ by Sartaj</span>
+        <span className='footer-bottom'>Maintained by @Sartaj</span>
+      </div>
     </div>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,28 +2,32 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import React from 'react'
-import { FaHome, FaUser, FaUserAlt } from "react-icons/fa";
+import { FaBriefcase, FaCogs, FaFileSignature, FaHeadphones, FaHome, FaUser, FaUserAlt } from "react-icons/fa";
 
 const Header = () => {
   const pathName = usePathname();
   console.log("pathName", pathName)
   return (
     <div className='px-5 flex justify-between bg-white'>
-      <h3 className='pointer brand-name'><span className='brand-name1'>SARTAJ</span> <span className='brand-name2'>ALAM</span></h3>
+      <h3 className='pointer brand-name'><span className='brand-name1'>SARTAJ</span><span className='brand-name2'>ALAM</span></h3>
       <div className='flex my-2'>
         <Link href={'/'}>
           <span className={`header-item${pathName === '/' ? "-active" : ""} flex`}> <FaHome className='mt-1 mx-1' style={{ fontSize: "23px" }} /> Home </span></Link>
         <Link href={'/about'}>
-          <span className={`header-item${pathName === '/about' ? "-active" : ""} mx-4 flex`}><FaUserAlt className='mt-2 mx-1' style={{ fontSize: "20px" }} /> About</span>
+          <span className={`header-item${pathName === '/about' ? "-active" : ""} mx-2 flex`}><FaUserAlt className='mt-2 mx-1' style={{ fontSize: "20px" }} /> About</span>
         </Link>
         <Link href={'/work'}>
-          <span className='header-item'>Work</span></Link>
+          <span className={`header-item${pathName === '/work' ? "-active" : ""} flex`}><FaBriefcase className='mt-2 mx-1' style={{ fontSize: "20px" }} /> Work</span>
+        </Link>
         <Link href={'/skills'}>
-          <span className='header-item mx-4'>Skills</span></Link>
+          <span className={`header-item${pathName === '/skills' ? "-active" : ""} mx-2 flex`}><FaCogs className='mt-2 mx-1' style={{ fontSize: "20px" }} /> Skills</span>
+        </Link>
         <Link href={'/projects'}>
-          <span className='header-item'>Projects</span></Link>
-        <Link href={'/contact'}>
-          <span className='header-item mx-4'>Contact</span></Link>
+          <span className={`header-item${pathName === '/projects' ? "-active" : ""} flex`}><FaFileSignature className='mt-2 mx-1' style={{ fontSize: "20px" }} /> Projects</span>
+        </Link>
+        <Link href={'/contacts'}>
+          <span className={`header-item${pathName === '/contacts' ? "-active" : ""} mx-2 flex`}><FaHeadphones className='mt-2 mx-1' style={{ fontSize: "20px" }} /> Contats</span>
+        </Link>
       </div>
     </div>
   )
