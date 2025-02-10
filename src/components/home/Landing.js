d7ee84836e20c5b257003b15ab18f165396b085a
sak03 <sartaj2394@gmail.com>
2025-02-10 13:33:41 +0530

    landing page updated

--- src/components/home/Landing.js
+++ src/components/home/Landing.js
@@ -1,11 +1,17 @@
 "use client"
-import React from 'react'
+import React, { useState } from 'react'
 import landingImg from '../../assets/landingImg/landing.jpg'
 import Image from 'next/image';
 import { Typewriter } from "react-simple-typewriter";
 
 const Landing = () => {
+    const [isNumberCopied, setIsNumberCopied] = useState(false);
 
+    const copyMobileNoToClipboard = () => {
+        navigator.clipboard.writeText("+919958564890");
+        setIsNumberCopied(true)
+        setTimeout(() => setIsNumberCopied(false), 2500)
+    };
 
     return (
         <div className='landing-section'>
@@ -28,9 +34,15 @@ const Landing = () => {
                 </p>
                 <div className='mt-5'>
                     <span className='resume-download'><a href='https://drive.google.com/file/d/11_B2NTzYyvlynrw9XSXtQIRMGbR8STKx/view' target='_blank'>MY RESUME</a></span>
-                    <span className='email-me'>EMAIL ME</span>
-                    <span className='call-me'>CALL ME</span>
+                    <a
+                        href="mailto:sartaj2394@gmail.com?subject=Want%20to%20work%20with%20us?&body=Hello%2C%20I%20would%20like%20to%20connect."
+                    > <span className='email-me'>EMAIL ME</span></a>
+                    <a href="tel:+911234567890" className='show-in-mobile-view'>
+                        <span className='call-me'>CALL ME</span>
+                    </a>
+                    <span className='call-me show-in-desktop-view' onClick={copyMobileNoToClipboard}>CALL ME</span>
                 </div>
+                <div className='mt-2'>{isNumberCopied ? <small className='text-danger my-2 p-2 show-in-desktop-view'>Phone number copied to clipboard!</small> : ""}</div>
             </div>
         </div>
     )
